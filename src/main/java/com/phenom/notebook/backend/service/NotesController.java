bc5f0235b30daf8150319e084a5187e3bc69c7a6
Can Ersoy <can.ersoy@tum.de>
2023-08-20 19:13:54 +0300

    cors fix

--- src/main/java/com/phenom/notebook/backend/service/NotesController.java
+++ src/main/java/com/phenom/notebook/backend/service/NotesController.java
@@ -2,12 +2,16 @@ package com.phenom.notebook.backend.service;
 
 import com.phenom.notebook.backend.model.Note;
 import com.phenom.notebook.backend.repository.NoteRepository;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoAction;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class NotesController {
 
@@ -19,15 +23,15 @@ public class NotesController {
         this.noteRepository = noteRepository;
     }
 
-    @GetMapping("/notes/{userId}")
-    public List<Note> getNotesByUser(@PathVariable String userId){
+     @GetMapping("/notes/{userId}")
+    public ResponseEntity<List<Note>> getNotesByUser(@PathVariable String userId){
         try{
-            return noteRepository.findNotesByUserId(userId);
+            return ResponseEntity.ok(noteRepository.findNotesByUserId(userId));
         }
         catch (Exception e){
             System.out.println(e.getMessage());
         }
-        return List.of();
+        return ResponseEntity.of(Optional.of(List.of()));
     }
 
     @PostMapping("/createNote/{id}/{userId}/{title}")
@@ -41,7 +45,7 @@ public class NotesController {
         }
     }
 
-    @PostMapping("/updateNote/{id}/{userId}/{title}")
+     @PostMapping("/updateNote/{id}/{userId}/{title}")
     public void updateNote(@PathVariable String id,@PathVariable String userId, @PathVariable String title, @RequestBody String text){
         try{
             noteRepository.deleteById(id);
