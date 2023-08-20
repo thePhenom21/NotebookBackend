31d78578d84123929f5d2e2eca026c03e70032f0
Can Ersoy <can.ersoy@tum.de>
2023-08-20 11:07:02 +0300

    db set up

---
+++ src/main/java/com/phenom/notebook/backend/model/Note.java
@@ -0,0 +1,15 @@
+package com.phenom.notebook.backend.model;
+
+
+import org.springframework.data.annotation.Id;
+
+
+public class Note {
+
+    private long id;
+    private String title;
+    private String text;
+    private String userId;
+
+
+}
